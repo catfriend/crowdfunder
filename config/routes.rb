b0630d291d38d0dcd5d9a17806363ed7649efc8c
catfriend <eakari@live.com>
2015-07-22 14:56:38 -0700

    added user interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  
   resources :users
 
   resources :projects do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717232616) do
+ActiveRecord::Schema.define(version: 20150722214458) do
 
   create_table "pledges", force: true do |t|
     t.string   "name"
